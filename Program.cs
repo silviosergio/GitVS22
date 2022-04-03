c88ef69e3eb9585e26ab7021183cc63ee5afed2e
Silvio <silvio.sergio@libero.it>
2022-04-04 01:31:31 +0200

    Prima versione funzionante

--- Program.cs
+++ Program.cs
@@ -7,6 +7,20 @@ namespace GitVS22
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Hanoi(3, "A", "B", "C");
+        }
+        static void Hanoi(int n, string from, string to, string via)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine($"spost un disco da {from} a {to}.");
+            }
+            else
+            {
+                Hanoi(n - 1, from, via, to);
+                Hanoi(1, from, to, via);
+                Hanoi(n - 1, via, to, from);
+            }
         }
     }
 }
