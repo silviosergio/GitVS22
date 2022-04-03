2e2867e8f42177c50e4698ee8b5ef6c133c50df4
Silvio <silvio.sergio@libero.it>
2022-04-04 01:22:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitVS22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
